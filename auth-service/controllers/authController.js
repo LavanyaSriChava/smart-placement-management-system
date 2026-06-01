0457d306836950cbb470bf5dad2282724da2ce2d
Deepika Valikala <deepikavalikala@gmail.com>
2026-06-01 15:43:01 +0530

    Integrated PostgreSQL with authentication service

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -1,41 +1,117 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const pool = require("../config/db");
 exports.register = async (req, res) => {
 
-    const { name, email, password } = req.body;
+    try {
 
-    const hashedPassword = await bcrypt.hash(
-        password,
-        10
-    );
-
-    res.json({
-        message: "User Registered",
-        user: {
+        const {
             name,
-            email
-        },
-        hashedPassword
-    });
+            email,
+            password,
+            role,
+            cgpa,
+            branch,
+            backlogs,
+            skills
+        } = req.body;
+
+        const existingUser = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword =
+            await bcrypt.hash(password, 10);
+
+        await pool.query(
+            `INSERT INTO users
+            (name,email,password,role,cgpa,branch,backlogs,skills)
+            VALUES($1,$2,$3,$4,$5,$6,$7,$8)`,
+            [
+                name,
+                email,
+                hashedPassword,
+                role,
+                cgpa,
+                branch,
+                backlogs,
+                skills
+            ]
+        );
+
+        res.status(201).json({
+            message: "User Registered Successfully"
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
 };
 exports.login = async (req, res) => {
 
-    const { email } = req.body;
+    try {
 
-    const token = jwt.sign(
-        {
-            email,
-            role: "student"
-        },
-        process.env.JWT_SECRET,
-        {
-            expiresIn: "1h"
+        const { email, password } = req.body;
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({
+                message: "User not found"
+            });
         }
-    );
 
-    res.json({
-        message: "Login Successful",
-        token
-    });
+        const user = result.rows[0];
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        );
+
+        res.json({
+            message: "Login Successful",
+            token
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
 };
