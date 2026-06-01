77c0fec6191b3002ed00ba91e61a4c5c153d9298
Deepika Valikala <deepikavalikala@gmail.com>
2026-06-01 16:19:44 +0530

    Implement notification service APIs

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const pool = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+
 dotenv.config();
 
 const app = express();
+
+// Database Connection
 pool.connect()
     .then(() => {
         console.log("Database Connected");
@@ -12,17 +19,23 @@ pool.connect()
     .catch((err) => {
         console.log("Database Error:", err.message);
     });
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
-
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/notifications", notificationRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
     res.send("Auth Service Running");
 });
 
+// Start Server
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server running on port ${process.env.PORT || 5000}`);
+    console.log(
+        `Server running on port ${process.env.PORT || 5000}`
+    );
 });
