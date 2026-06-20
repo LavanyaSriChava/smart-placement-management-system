e1c68dfb83332fb804ba41d95625950ed4f8b5d6
Deepika Valikala <deepikavalikala@gmail.com>
2026-06-20 16:08:41 +0530

    chore: switch frontend companyAPIs to localhost for local integration testing

--- frontend/src/api/companyApi.js
+++ frontend/src/api/companyApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  "https://bruna-subvertebral-illustriously.ngrok-free.dev/api/companies";
+const API_URL = "http://localhost:8080/api/companies";
 
 export const getCompanies = async () => {
   const response = await axios.get(API_URL);
