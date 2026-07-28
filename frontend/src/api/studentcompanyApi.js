70f9b38894f890b6fa24414a0690a6c9428352fd
LavanyaSriChava <lavanya.chava728@gmail.com>
2026-07-29 01:06:08 +0530

    Remove accidentally committed student frontend files

--- frontend/src/api/studentcompanyApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL =
-  "https://bruna-subvertebral-illustriously.ngrok-free.dev/api/companies";
-
-export const getCompanies = () => {
-  return axios.get(API_URL);
-};
